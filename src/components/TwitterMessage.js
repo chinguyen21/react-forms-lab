3c1148d860517312c6d827017e02fc64ff2021ef
Chi Nguyen <nguyen.thimai@northeastern.edu>
2021-02-04 20:47:01 -0500

    Done.

--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -4,14 +4,26 @@ class TwitterMessage extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      message: ""
+    };
+  }
+
+
+  handleChange = (event) => {
+    // event.preventDefault();
+    this.setState({
+      [event.target.name]: event.target.value
+    })
+ 
   }
 
   render() {
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" name="message" id="message" />
+        <input onChange={this.handleChange}type="text" name="message" id="message" value={this.state.message} />
+        <p>Remaining Characters: {this.props.maxChars-this.state.message.length}/{this.props.maxChars}</p>
       </div>
     );
   }
